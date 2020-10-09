1e382d8bc305956ab8c382883fcfe712aa71b909
Thomas Monfre <tmonfre1@gmail.com>
2020-10-09 14:13:35 -0400

    chore: spacing

--- src/services/google.py
+++ src/services/google.py
@@ -6,17 +6,14 @@ GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 
 def get_election_info():
     URL = BASE_URL + "elections"
-
-    PARAMS = {'key':GOOGLE_API_KEY} 
+    PARAMS = { 'key': GOOGLE_API_KEY } 
     
     response = requests.get(url = URL, params = PARAMS) 
     data = response.json() 
-
     return data
 
 def get_voter_info(address, electionId):
     URL = BASE_URL + "voterinfo"
-
     PARAMS = {
         'key': GOOGLE_API_KEY,
         'address': address,
@@ -25,12 +22,10 @@ def get_voter_info(address, electionId):
     
     response = requests.get(url = URL, params = PARAMS) 
     data = response.json() 
-
     return data
 
 def get_representative_info(address):
     URL = BASE_URL + "representatives"
-
     PARAMS = {
         'key': GOOGLE_API_KEY,
         'address': address
@@ -38,5 +33,4 @@ def get_representative_info(address):
     
     response = requests.get(url = URL, params = PARAMS) 
     data = response.json() 
-
     return data
